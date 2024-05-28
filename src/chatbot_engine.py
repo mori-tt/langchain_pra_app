313b0ff910d997d2367a320e64854f87af124e52
T_Mori <morit0301@gmail.com>
2024-05-28 17:40:30 +0900

    ✨ プライベートな文書を検索する機能の実装

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -2,12 +2,42 @@ from langchain.chat_models import ChatOpenAI
 import langchain
 from langchain.memory import ChatMessageHistory
 from langchain.schema import HumanMessage
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo,
+)
+from typing import List
+from langchain.tools import BaseTool
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import initialize_agent, AgentType
+
 
 langchain.verbose = True
 
-def chat(message: str, history: ChatMessageHistory) -> str:
+def create_index() -> VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", glob="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
+
+def create_tools(index: VectorStoreIndexWrapper) -> List[BaseTool]:
+    vectorstore_info = VectorStoreInfo(
+    name="langchain_pra_app code",
+    description="Source code of application named langchain_pra_app",
+    vectorstore=index.vectorstore,
+    )
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    return toolkit.get_tools()
+
+def chat(message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    messages = history.messages
-    messages.append(HumanMessage(content=message))
-    return llm(messages).content
+    tools = create_tools(index)
+
+    memory = ConversationBufferMemory(chat_memory=history, memory_key="chat_history", return_messages=True)
+
+    agent_chain = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, memory=memory)
+
+    return agent_chain.run(input=message)
--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 from dotenv import load_dotenv
-from chatbot_engine import chat
+from chatbot_engine import chat, create_index
 from langchain.memory import ChatMessageHistory
 import os
 
@@ -10,7 +10,7 @@ def respond(message, chat_history):
         history.add_user_message(user_message)
         history.add_ai_message(ai_message)
     
-    bot_message = chat(message, history)
+    bot_message = chat(message, history, index)
     chat_history.append((message, bot_message))
     return "", chat_history
 
@@ -34,4 +34,5 @@ if __name__ == "__main__":
     else:
         auth = None
 
+    index = create_index()
     demo.launch(auth=auth)
